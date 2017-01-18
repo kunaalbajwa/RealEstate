2174c2fb75d8836dc9449824f12ebb41a9b21ed4
kunaa <kunaa@DESKTOP-OITTOC1>
2017-01-18 11:17:47 -0600

    first commit

---
+++ src/realestate/Property.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package realestate;
+
+/**
+ *
+ * @author kunaa
+ */
+public class Property {
+    double rent;
+    String propertyName;
+    String address;
+    int number;
+    //use this as a boolean (mentally) to default to 0 to show if it is an aparment or not
+}
